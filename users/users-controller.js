da7b69bf0eb36fd15a5e4f43f3f03d4e324c31a9
Phway Thant Chae <ilovejazz555@gmail.com>
2023-06-11 18:02:26 -0400

    section 4 done

--- users/users-controller.js
+++ users/users-controller.js
@@ -5,6 +5,8 @@ const UserController = (app) => {
   app.get("/api/users", findUsers);
   app.get("/api/users/:uid", findUserById);
   app.post("/api/users", createUser);
+  app.delete("/api/users/:uid", deleteUser);
+  app.put("/api/users/:uid", updateUser);
 };
 const findUsers = (req, res) => {
   const type = req.query.type;
@@ -17,16 +19,31 @@ const findUsers = (req, res) => {
 };
 
 const createUser = (req, res) => {
-    const newUser = req.body;
-    newUser._id = (new Date()).getTime() + '';
-    users.push(newUser);
-    res.json(newUser);
-}
+  const newUser = req.body;
+  newUser._id = new Date().getTime() + "";
+  users.push(newUser);
+  res.json(newUser);
+};
 
 const findUserById = (req, res) => {
   const userId = req.params.uid;
   const user = users.find((u) => u._id === userId);
   res.json(user);
+};
+
+const deleteUser = (req, res) => {
+  const userId = req.params["uid"];
+  users = users.filter((usr) => usr._id !== userId);
+  res.sendStatus(200);
+};
+
+const updateUser = (req, res) => {
+  const userId = req.params["uid"];
+  const updates = req.body;
+  users = users.map((usr) =>
+    usr._id === userId ? { ...usr, ...updates } : usr
+  );
+  res.sendStatus(200);
 };
 
 export default UserController;
