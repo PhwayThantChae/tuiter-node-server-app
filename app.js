d9963f8702b0581cff8dfaf2616a6edf2d17ba2a
Phway Thant Chae <ilovejazz555@gmail.com>
2023-06-12 04:45:17 -0400

    7 done

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
-import express from 'express';
+import express from "express";
+import session from "express-session";
 import HelloController from "./controllers/hello-controller.js";
 import UserController from "./users/users-controller.js";
-import tuitsController from './controllers/tuits/tuits-controller.js';
-import cors from 'cors';
+import AuthController from "./users/auth-controller.js";
+import tuitsController from "./controllers/tuits/tuits-controller.js";
+import cors from "cors";
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.json());
+app.use(
+  session({
+    secret: "any string",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 tuitsController(app);
 HelloController(app);
 UserController(app);
+AuthController(app);
 app.listen(4000);
