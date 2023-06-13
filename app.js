3f5c27c5bc35ac27efbaa1a1133eb8a394dd0a3f
Phway Thant Chae <ilovejazz555@gmail.com>
2023-06-12 20:16:26 -0400

    update url

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://a5--magenta-starship-4926eb.netlify.app"],
   })
 );
 app.use(express.json());
